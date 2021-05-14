2f6f0f81ae344cf0346dc77ffe1f7cf9c768ad4b
Luke Teal <l.teal21@gmail.com>
2021-05-14 11:18:42 -0400

    add clarity to employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,22 +2,22 @@ const Employee = require ('../lib/Employee.js')
 
 describe('Employee', () => {
 
-    it('should return the name of the employee', () => {
+    it('should return the name of the employee with getName', () => {
         const john = new Employee('John Doe', 'J1', 'john@email.com')
         expect(john.getName()).toBe('John Doe')
     })
 
-    it('should return the id of the employee', () => {
+    it('should return the id of the employee with getId', () => {
         const john = new Employee('John Doe', 'J1', 'john@email.com')
         expect(john.getId()).toBe('J1')
     })
 
-    it('should return the email of the employee', () => {
+    it('should return the email of the employee with getEmail', () => {
         const john = new Employee('John Doe', 'J1', 'john@email.com')
         expect(john.getEmail()).toBe('john@email.com')
     })
 
-    it('should return the role of the employee', () => {
+    it('should return the role of the employee with getRole', () => {
         const john = new Employee('John Doe', 'J1', 'john@email.com')
         expect(john.getRole()).toBe('Employee')
     })
